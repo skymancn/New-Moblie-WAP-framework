67c64a5cc4c05d4d04f43303bea825e95dcf3669
jiey <jiey@ctrip.com>
2014-11-14 18:39:20 +0800

    add ionic some methods

--- coco/coco-app/js/main.js
+++ coco/coco-app/js/main.js
@@ -4,6 +4,10 @@
 
 var __LIBS_BASE__ = '/libs/', // js libs 内部库的baseUrl
     __3RD_LIBS_BASE__ = '/libs/3rd-libs/'; // 第三方库的baseUrl
+
+// 全局变量给scrollView用的
+require.scroll = {};
+
 require.config({
     baseUrl: '/coco-app/js/',
     shim: {
@@ -22,7 +26,19 @@ require.config({
         'local-storage': __LIBS_BASE__ + 'common/local-storage/local-storage-v0.1.0',
         'keyword-store': __LIBS_BASE__ + 'common/keyword-store/keyword-store-v0.1.0',
         'geolocation': __LIBS_BASE__ + 'common/geolocation/geolocation-v0.1.0',
-        'loading': __LIBS_BASE__ + 'ui/loading/loading-v0.1.0'
+        'loading': __LIBS_BASE__ + 'ui/loading/loading-v0.1.0',
+
+        'dom-utils': __LIBS_BASE__ + 'utils/dom-utils/dom-utils-v0.1.0',
+        'activator': __LIBS_BASE__ + 'utils/activator/activator-v0.1.0',
+        'tap': __LIBS_BASE__ + 'utils/tap/tap-v0.1.0',
+        'utils-events': __LIBS_BASE__ + 'utils/events/events-v0.1.0',
+        'platform': __LIBS_BASE__ + 'utils/platform/platform-v0.1.0',
+        'utils': __LIBS_BASE__ + 'utils/utils/utils-v0.1.0',
+        'viewport': __LIBS_BASE__ + 'utils/viewport/viewport-v0.1.0',
+        'css': __LIBS_BASE__ + 'utils/css/css-v0.1.0',
+
+        'views-view': __LIBS_BASE__ + 'views/view/view-v0.1.0',
+        'side-menu-view': __LIBS_BASE__ + 'views/side-menu-view/side-menu-view-v0.1.0',
     }
 });
 
